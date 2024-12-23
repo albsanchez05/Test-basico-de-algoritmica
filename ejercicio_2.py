23c7e268611b3e53e6019223a3cbfc51c16b2ef9
Alberto Sanchez <120468316+albsanchez05@users.noreply.github.com>
2024-12-23 11:26:22 +0100

    added total_personas var, removed comments

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,8 +1,9 @@
 def main():
     i = 1
+    total_personas = 4
     personas = []
     print("LEER PERSONAS:")
-    while i < 51:
+    while i < total_personas + 1:
 
         print(f"Persona {i}:")
         try:
@@ -15,42 +16,38 @@ def main():
         except ValueError:
             print('Error: el sexo debe ser indicado con M o F, y la edad debe ser mayor o igual a 0')
             continue
-       	personas.append({"sexo": sexo, "edad": edad})
+        personas.append({"sexo": sexo, "edad": edad})
         i = i + 1
 
     mayores_edad = 0
     menores_edad = 0
-    masculinos_mayores = 0
-    femeninas_menores = 0
+    hombres_mayores = 0
+    mujeres_menores = 0
     mujeres = 0
 
-    # Procesar los datos
     for persona in personas:
         if persona["edad"] >= 18:
             mayores_edad += 1
             if persona["sexo"] == "M":
-                masculinos_mayores += 1
+                hombres_mayores += 1
         else:
             menores_edad += 1
             if persona["sexo"] == "F":
-                femeninas_menores += 1
+                mujeres_menores += 1
 
         if persona["sexo"] == "F":
             mujeres += 1
 
-    # Calcular porcentajes
-    porcentaje_mayores = (mayores_edad / 50) * 100
-    porcentaje_mujeres = (mujeres / 50) * 100
+    porcentaje_mayores = (mayores_edad / total_personas) * 100
+    porcentaje_mujeres = (mujeres / total_personas) * 100
 
-    # Mostrar resultados
     print("\nResultados:")
     print(f"a. Cantidad de personas mayores de edad: {mayores_edad}")
     print(f"b. Cantidad de personas menores de edad: {menores_edad}")
-    print(f"c. Cantidad de hombres mayores de edad: {masculinos_mayores}")
-    print(f"d. Cantidad de mujeres menores de edad: {femeninas_menores}")
+    print(f"c. Cantidad de hombres mayores de edad: {hombres_mayores}")
+    print(f"d. Cantidad de mujeres menores de edad: {mujeres_menores}")
     print(f"e. Porcentaje de personas mayores de edad: {porcentaje_mayores:.2f}%")
     print(f"f. Porcentaje de mujeres respecto al total: {porcentaje_mujeres:.2f}%")
 
-# Ejecutar el programa
 if __name__ == "__main__":
     main()
